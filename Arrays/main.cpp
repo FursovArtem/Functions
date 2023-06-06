d503ab263ba282ea32d75686b31bcdac0033ec24
Artem <aztec@DESKTOP-CK171NA>
2023-06-06 20:24:01 +0400

    Add project files.

---
+++ Arrays/main.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+using namespace std;
+
+void fill_rand(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void print(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+
+void sort(int arr[], const int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+int sum(int arr[], const int size)
+{
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+double avg(int arr[], const int size)
+{
+	return sum(arr, size) / (double)size;
+}
+
+int minvalue(int arr[], const int size)
+{
+	int min = arr[0];
+	for (int i = 1; i < size; i++)
+	{
+		min > arr[i] ? min = arr[i] : NULL;
+	}
+	return min;
+}
+
+int maxvalue(int arr[], const int size)
+{
+	int max = arr[0];
+	for (int i = 1; i < size; i++)
+	{
+		max < arr[i] ? max = arr[i] : NULL;
+	}
+	return max;
+}
+
+void shiftleft(int arr[], const int size, int shift)
+{
+	for (int i = 0; i < shift; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			swap(arr[j + 1], arr[j]);
+		}
+	}
+}
+
+void shiftright(int arr[], const int size, int shift)
+{
+	for (int i = 0; i < shift; i++)
+	{
+		for (int j = size - 1; j > 0; j--)
+		{
+			swap(arr[j], arr[j - 1]);
+		}
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int size = 10;
+	int arr[size]{};
+
+	cout << "Инициализация" << endl;
+	print(arr, size);
+	cout << endl;
+
+	cout << "Заполнение рандомными числами" << endl;
+	fill_rand(arr, size);
+	print(arr, size);
+	cout << endl;
+
+	cout << "Сортировка" << endl;
+	sort(arr, size);
+	print(arr, size);
+	cout << endl;
+
+	cout << "Сумма элементов = " << sum(arr, size) << endl;
+	cout << "Среднее арифметическое = " << avg(arr, size) << endl;
+	cout << "Минимальное значение: " << minvalue(arr, size) << endl;
+	cout << "Максимальное значение: " << maxvalue(arr, size) << endl;
+
+	int sh;
+	cout << "Сдвиг влево на: "; cin >> sh;
+	shiftleft(arr, size, sh);
+	print(arr, size);
+	cout << endl;
+
+	cout << "Сдвиг вправо на: "; cin >> sh;
+	shiftright(arr, size, sh);
+	print(arr, size);
+	cout << endl;
+}
---
+++ Functions/main.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+
+double calc(int a, char s, int b)
+{
+	switch (s)
+	{
+	case '+': return a + b;
+	case '-': return a - b;
+	case '*': return a * b;
+	case '/':
+		if (b)
+		{
+			return double(a) / b;
+		}
+		throw "Division by zero";
+	case '^':
+		if (b > 0)
+		{
+			int power = a;
+			for (int i = 1; i < b; i++)
+			{
+				a *= power;
+			}
+			return a;
+		}
+		else if (b < 0)
+		{
+			int power = a;
+			for (int i = -1; i > b; i--)
+			{
+				a *= power;
+			}
+			return 1 / (double)a;
+
+		}
+		else
+		{
+			return 1;
+		}
+	}
+}
+
+int factorial(int a)
+{
+	if (a <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return a * factorial(a - 1);
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	char s;
+	cout << "Введите выражение: "; cin >> a >> s >> b;
+	try
+	{
+		cout << a << " " << s << " " << b << " = " << calc(a, s, b) << endl;
+		//cout << a << " ! = " << factorial(a) << endl;
+	}
+	catch (const char* error_message)
+	{
+		cout << error_message << endl;
+	}
+}
