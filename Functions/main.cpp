4da19fcaa9e200d3a50222603fac7d6aed00a0b1
Artem <aztec@DESKTOP-CK171NA>
2023-06-13 18:04:58 +0400

    calc

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -43,14 +43,7 @@ double calc(int a, char s, int b)
 
 int factorial(int a)
 {
-	if (a <= 1)
-	{
-		return 1;
-	}
-	else
-	{
-		return a * factorial(a - 1);
-	}
+	return a <= 1 ? 1 : a * factorial(a - 1);
 }
 
 void main()
@@ -58,11 +51,11 @@ void main()
 	setlocale(LC_ALL, "");
 	int a, b;
 	char s;
-	cout << "Введите выражение: "; cin >> a >> s >> b;
+	cout << "Введите выражение: "; cin >> a;
 	try
 	{
-		cout << a << " " << s << " " << b << " = " << calc(a, s, b) << endl;
-		//cout << a << " ! = " << factorial(a) << endl;
+		//cout << a << " " << s << " " << b << " = " << calc(a, s, b) << endl;
+		cout << a << " ! = " << factorial(a) << endl;
 	}
 	catch (const char* error_message)
 	{
