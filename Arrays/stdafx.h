e4d0462c3c72e667b7d37decec17bbf819030b92
Artem <aztec@DESKTOP-CK171NA>
2023-06-29 16:34:53 +0400

    headers

--- Arrays/stdafx.h
+++ Arrays/stdafx.h
@@ -1,4 +1,6 @@
 ﻿//stdafx.h - precompiled header (pch.h)
 #pragma once
+//#pragma once - эта директива компоновщика позволяет включить файл в сборку только один раз
+//независимо от количества подключений (от количества #include -ов файла)
 #include <iostream>
 using namespace std;
